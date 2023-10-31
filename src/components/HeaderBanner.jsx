fa95f723a581dbb6df114da6d3027666092c8b79
Carlos <rafailan99@gmail.com>
2023-10-31 15:18:13 -0600

    Fixed some things

--- src/components/HeaderBanner.jsx
+++ src/components/HeaderBanner.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { VscChevronRight, VscChevronLeft } from 'react-icons/vsc';
-import slide1 from '../images/slide1.png'
-import slide2 from '../images/slide2.png'
-import slide3 from '../images/slide3.png'
-import slide4 from '../images/slide4.png'
+import slide1 from '../images/slide1.jpg'
+import slide2 from '../images/slide2.jpg'
+import slide3 from '../images/slide3.jpg'
+import slide4 from '../images/slide4.jpg'
+import slide5 from '../images/slide5.jpg'
 
 
 function HeaderBanner() {
@@ -20,6 +21,9 @@ function HeaderBanner() {
         },
         {
             url: slide4
+        },
+        {
+            url: slide5
         },
     ];
     const [currentIndex, setCurrentIndex] = useState(0);
