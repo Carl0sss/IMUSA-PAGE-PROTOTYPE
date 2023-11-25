98536b443e1d53e14be46a3052fd90a96c6edb81
Carlos <rafailan99@gmail.com>
2023-11-25 08:29:12 -0600

    Some changes

--- src/components/WidgetWrap.jsx
+++ src/components/WidgetWrap.jsx
@@ -6,27 +6,22 @@ function WidgetWrap() {
     return (
         <div className="fixed top-1/4 right-4 transform -translate-y-1/4 bg-amber-700 w-auto rounded-lg">
             <div className="flex flex-col items-center space-y-4 text-white h-full">
-                <div className='w-full h-full hover:bg-amber-600 p-2'>
-                <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                <a className='w-full h-full hover:bg-amber-600 p-2 hover:rounded-t-lg' href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
                     <BiLogoFacebookSquare size={30} />
                 </a>
-                </div>
-                <div className='w-full h-full hover:bg-amber-600 p-2'>
-                <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+                <a className='w-full h-full hover:bg-amber-600 p-2' href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
                     <CiInstagram size={30} />
                 </a>
-                </div>
-                <div className='w-full h-full hover:bg-amber-600 p-2'>
-                <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                <a className='w-full h-full hover:bg-amber-600 p-2' href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
                     <BiLogoWhatsapp size={30} />
                 </a>
-                </div>
                 <hr className='border-b-0 border-white w-2/3 mx-auto' />
                 <div className='flex items-center h-20'>
                     <span className='text-center -rotate-90 mt-2 w-8'>Síguenos</span>
                 </div>
             </div>
         </div>
+
     )
 }
 
