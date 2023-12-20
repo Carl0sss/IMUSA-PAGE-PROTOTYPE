fbcc7a5fa41ffe3852d13fa6eaf2e96831b36483
Carlos <rafailan99@gmail.com>
2023-12-20 09:42:19 -0600

    Add collage component and other features

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -22,29 +22,27 @@ function Carousel({ images }) {
     };
 
     return (
-        <div>
-            <div className='min-w-[620px] h-[480px] w-full m-auto py-5 relative group z-0'>
-                <div
-                    style={{ backgroundImage: `url(${images[currentIndex]})` }}
-                    className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
-                ></div>
-                <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-                    <VscChevronLeft onClick={prevSlide} size={30} />
-                </div>
-                <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-                    <VscChevronRight onClick={nextSlide} size={30} />
-                </div>
-                <div className='flex top-4 justify-center py-2'>
-                    {images.map((image, index) => (
-                        <div
-                            key={index}
-                            onClick={() => goToSlide(index)}
-                            className='text-2xl cursor-pointer'
-                        >
-                            <RxDotFilled />
-                        </div>
-                    ))}
-                </div>
+        <div className='min-w-[620px] h-[480px] w-full m-auto py-5 relative group z-0'>
+            <div
+                style={{ backgroundImage: `url(${images[currentIndex]})` }}
+                className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+            ></div>
+            <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+                <VscChevronLeft onClick={prevSlide} size={30} />
+            </div>
+            <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+                <VscChevronRight onClick={nextSlide} size={30} />
+            </div>
+            <div className='flex top-4 justify-center py-2'>
+                {images.map((image, index) => (
+                    <div
+                        key={index}
+                        onClick={() => goToSlide(index)}
+                        className='text-2xl cursor-pointer'
+                    >
+                        <RxDotFilled />
+                    </div>
+                ))}
             </div>
         </div>
     )
