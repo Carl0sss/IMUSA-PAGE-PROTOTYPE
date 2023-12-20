afdf3ab58894a9e55e76efe96af562326452446c
Carlos <rafailan99@gmail.com>
2023-12-20 09:44:38 -0600

    A few changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -53,7 +53,7 @@ function Footer() {
               <a href='https://www.facebook.com/people/Inmusa/61554061884844/' className='text-amber-700 px-1 hover:text-gray-500' target='_blank' rel='noopener noreferrer'>
                 <BiLogoFacebookSquare size={30} />
               </a>
-              <a href="/" className='text-amber-700 px-1 hover:text-gray-500' target='_blank' rel='noopener noreferrer'>
+              <a href='https://www.instagram.com/insumosmultiples.sv/' className='text-amber-700 px-1 hover:text-gray-500' target='_blank' rel='noopener noreferrer'>
                 <CiInstagram size={30} />
               </a>
               <a href="/" className='text-amber-700 px-1 hover:text-gray-500' target='_blank' rel='noopener noreferrer'>
